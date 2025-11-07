aa15079be2cbbfcd40b0455f1dc9733aaccdf519
evertonk <evertonluizcoster@gmail.com>
2025-11-07 17:06:29 -0300

    feat: add chadcn ui components

---
+++ src/components/ui/currencySymbol/CurrencySymbol.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { CurrencySymbolType } from '@/lib/types/commonTypes';
+
+interface Props {
+  symbol: CurrencySymbolType
+  sizeClass?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  sizeClass: 'text-3xl font-medium'
+})
+</script>
+
+<template>
+  <span 
+    class="bill-switch__label" 
+    :class="[props.sizeClass]"
+  >
+    {{ props.symbol }}
+  </span>
+</template>
---
+++ src/components/ui/switch/Switch.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { reactiveOmit } from '@vueuse/core'
+import { SwitchRoot, SwitchThumb, useForwardPropsEmits } from 'reka-ui'
+import { cn } from '@/lib/utils'
+
+const props = defineProps({
+  defaultValue: { type: Boolean, required: false },
+  modelValue: { type: [Boolean, null], required: false },
+  
+  disabled: { type: Boolean, required: false },
+  id: { type: String, required: false },
+  value: { type: String, required: false },
+  asChild: { type: Boolean, required: false },
+  as: { type: null, required: false },
+  name: { type: String, required: false },
+  required: { type: Boolean, required: false },
+  class: { type: null, required: false },
+})
+
+const emits = defineEmits(['update:modelValue'])
+
+const delegatedProps = reactiveOmit(props, 'class')
+
+const forwarded = useForwardPropsEmits(delegatedProps, emits)
+</script>
+
+<template>
+  <SwitchRoot
+    data-slot="switch"
+    v-bind="forwarded"
+    :class="
+      cn(
+        'peer data-[state=checked]:bg-primary data-[state=unchecked]:bg-input focus-visible:border-ring focus-visible:ring-ring/50 dark:data-[state=unchecked]:bg-input/80 inline-flex h-8 w-14 shrink-0 items-center border border-transparent shadow-xs transition-all outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50',
+        props.class
+      )
+    "
+  >
+    <SwitchThumb
+      data-slot="switch-thumb"
+      :class="
+        cn(
+          'bg-background dark:data-[state=unchecked]:bg-foreground dark:data-[state=checked]:bg-primary-foreground pointer-events-none block size-7 ring-0 transition-transform data-[state=checked]:translate-x-[calc(100%-2px)] data-[state=unchecked]:translate-x-0',
+        )
+      "
+    >
+       <slot name="thumb" />
+    </SwitchThumb>
+  </SwitchRoot>
+</template>
