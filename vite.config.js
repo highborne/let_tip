cd26aa16a45aebf644661a5d3673b472b4fa3eab
evertonk <evertonluizcoster@gmail.com>
2025-11-08 15:46:59 -0300

    chore: add test configs

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
+  test: {
+    globals: true,        
+    environment: 'jsdom', 
+    setupFiles: './src/tests/setup.ts',
+    coverage: {
+      reporter: ['text', 'html'],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
